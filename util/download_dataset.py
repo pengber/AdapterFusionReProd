4188a237dbb9a32a896f3a9c9b4aff70e221ad67
pengber <pengber@126.com>
2023-11-01 11:16:16 +0800

    修改下载数据集代码

--- util/download_dataset.py
+++ util/download_dataset.py
@@ -1,7 +1,5 @@
 
-# from datasets import load_dataset
-# dataset = load_dataset('imdb')
-# dataset.save_to_disk('./data')
+from datasets import load_dataset
+dataset = load_dataset('imdb')
+dataset.save_to_disk('./data')
 
-import tensorflow_datasets as tfds
-tfds.list_builders()
